2b39fc325ef684abc70f99b326d9d5a509575fb7
JongSeung Kim <jngsngkm@gmail.com>
2022-07-25 13:06:50 +0000

    이진탐색

---
+++ main.py
@@ -0,0 +1,21 @@
+def binary_serch(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_serch(my_list, 3))
+print(binary_serch(my_list, -1))
