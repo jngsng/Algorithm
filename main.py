a88fce9dec18564b9ed5151f9a74aedefaa563d1
JongSeung Kim <jngsngkm@gmail.com>
2022-07-27 14:01:58 +0000

    선택정렬

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-def binary_serch(list, item):
-    low = 0
-    high = len(list) - 1
+def findSmallest(arr):
+  smallest = arr[0]
+  smallest_index = 0
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest = arr[i]
+      smallest_index = i
+  return smallest_index
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = list[mid]
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+    smallest = findSmallest(arr)
+    newArr.append(arr.pop(smallest))
+  return newArr
 
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
-
-
-my_list = [1, 3, 5, 7, 9]
-
-print(binary_serch(my_list, 3))
-print(binary_serch(my_list, -1))
+print(selectionSort([5,3,6,2,10]))
