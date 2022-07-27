4585da9eba359c623e5045608cf3bca33b9c2dbc
JongSeung Kim <jngsngkm@gmail.com>
2022-07-27 14:11:04 +0000

    기본 재귀함수

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-def findSmallest(arr):
-  smallest = arr[0]
-  smallest_index = 0
-  for i in range(1, len(arr)):
-    if arr[i] < smallest:
-      smallest = arr[i]
-      smallest_index = i
-  return smallest_index
+def countdown(i):
+    print(i)
+    if i <= 1:
+        return
+    else:
+        countdown(i - 1)
 
-def selectionSort(arr):
-  newArr = []
-  for i in range(len(arr)):
-    smallest = findSmallest(arr)
-    newArr.append(arr.pop(smallest))
-  return newArr
 
-print(selectionSort([5,3,6,2,10]))
+countdown(5)
