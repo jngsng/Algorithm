e4a86e1f7eb60f31a2b5107905fd1ff4a49051ec
JongSeung Kim <jngsngkm@gmail.com>
2022-07-30 14:47:06 +0000

    해시테이블

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def quicksort(array):
-  if len(array) < 2:
-    return array
-  else:
-    pivot = array[0]
-    less = [i for i in array[1:] if i <= pivot]
-    greater = [i for i in array[1:] if i> pivot]
-    return quicksort(less) + [pivot] + quicksort(greater)
+book = dict()
 
-print(quicksort([10,5,2,3]))
+book["apple"] = 0.67
+book["milk"] = 1.49
+book["abocado"] = 1.49
+
+print(book)
+
+print(book["abocado"])
