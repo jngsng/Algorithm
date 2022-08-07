e34265d1794522801ceb82902734d844366b5ce8
JongSeung Kim <jngsngkm@gmail.com>
2022-08-07 13:49:27 +0000

    다익스트라 알고리즘

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
-from collections import deque
-
 graph = {}
-graph["you"] = ["allice", "bob", "claire", "mm"]
+#graph["you"] = ["alice","bob","claire"]
 
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
 
-def person_is_seller(name):
-    return (name[-1]) == 'm'
+graph["a"] = {}
+graph["a"]["fin"] = 1
 
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
 
-def search_name(name):
-    search_queue = deque()
-    search_queue += graph[name]
-    searched = []
+graph["fin"] = {}
 
-    while search_queue:
-        person = search_queue.popleft()
-        if not person in searched:
-            if person_is_seller(person):
-                print(person + " is a mango seller")
-                return True
-        else:
-            search_queue += graph[person]
-            searched.append(person)
+infinity = float("inf")
 
-    return False
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
 
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
 
-search_name("you")
+processed = []
+
+def find_lowest_cost_node(costs):
+  lowest_cost = float("inf")
+  lowest_cost_node = None
+  for node in costs:
+    cost = costs[node]
+    if cost < lowest_cost and node not in processed:
+      lowest_cost = cost
+      lowest_cost_node = node
+  return lowest_cost_node
+  
+node = find_lowest_cost_node(costs)
+
+while node is not None:
+  cost = costs[node]
+  neighbors = graph[node]
+  for n in neighbors.keys():
+    new_cost = cost + neighbors[n]
+    if costs[n] > new_cost:
+      costs[n] = new_cost
+      parents[n] = node
+  processed.append(node)
+  node = find_lowest_cost_node(costs)
