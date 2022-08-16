2d5d0f6c4ecc467f4ee01cf1bc03050d0dae3b0a
JongSeung Kim <jngsngkm@gmail.com>
2022-08-16 14:30:04 +0000

    가장많이 포함된 알파벳 갯수 구하기

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-word = "test"
+word = "qweklbrwklbfklsdanflnasdfklasdfnklasdnflk"
 
 
 def letter_count(word):
   map = {}
+  most = {}
   for alphabet in word:
     if map.get(alphabet) == None:
       map[alphabet] = 1
     else:
       map[alphabet] += 1
 
-      print("map => ", map)
-  return -1
+  max = -1
+  for key in map:
+    if map[key] > max:
+      max = map[key]
+      
+  return max
 
 
 print(letter_count(word))
